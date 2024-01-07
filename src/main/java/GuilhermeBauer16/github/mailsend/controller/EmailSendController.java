36c817659cb1ee6bf1ec574150f8104db0944f53
guilherme <guilhermebauer16@gmail.com>
2024-01-07 00:19:17 -0300

    feat: add mail send functions

---
+++ src/main/java/GuilhermeBauer16/github/mailsend/controller/EmailSendController.java
@@ -0,0 +1,27 @@
+package GuilhermeBauer16.github.mailsend.controller;
+
+import GuilhermeBauer16.github.mailsend.service.EmailService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.security.auth.Subject;
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSendController {
+
+    private EmailService emailService;
+
+    public EmailSendController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public String sendMail(@RequestParam(value = "file", required = false)MultipartFile[] file, String  to ,
+                           String[] cc , String subject , String body){
+        return emailService.sendMail(file,to,cc,subject,body);
+    }
+}
